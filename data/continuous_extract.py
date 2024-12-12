c1fa6d48a965494c6f1c1b096af4db83b7c20736
jun <gustavonagatomo@gmail.com>
2024-12-12 17:29:07 +0900

    feat: Metadata file for each request

--- data/continuous_extract.py
+++ data/continuous_extract.py
@@ -28,8 +28,9 @@ def cnt_extract(
         print(f"Target directory for continuous data does not exist yet, created at {target_dir}")
 
     # the name of the target dir is the date of the extraction request
-    target_dir += "/" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    os.mkdir(target_dir)
+    request_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    target_dir = os.path.join(target_dir, request_timestamp)
+    os.makedirs(target_dir)
     os.chdir(target_dir)
 
     # network code is 0101 for HiNet
@@ -38,5 +39,11 @@ def cnt_extract(
     win32.extract_sac(data, ctable) 
 
     win32.extract_sacpz(ctable)
+
+    with open("info.txt", "w") as f:
+        f.write("Request Timestamp: " + str(request_timestamp) + "\n")
+        f.write("Start Time: " + str(starttime) + "\n")
+        f.write("End Time: " + str(starttime + timedelta(minutes=span)) + "\n")
+        f.write("Time Span: " + str(span) + ("minute" if span == 1 else "minutes") + "\n")
 
     os.chdir(project_root)
