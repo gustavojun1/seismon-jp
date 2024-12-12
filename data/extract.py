89f337915b1d272ba1487cd82373d483048c4359
jun <gustavonagatomo@gmail.com>
2024-12-12 18:59:57 +0900

    feat: Modularize metadata file creation

--- data/extract.py
+++ data/extract.py
@@ -17,6 +17,15 @@ def createTargetDir(project_root: str, data_type: Literal["continuous", "event"]
     os.makedirs(target_dir)
     os.chdir(target_dir)
 
+def createMetadataFile(request_timestamp, start_time, span, end_time: bool = True, name = "info.txt"):
+
+    with open(name, "w") as f:
+        f.write("Request Timestamp: " + str(request_timestamp) + "\n")
+        f.write("Start Time: " + str(start_time) + "\n")
+        if end_time:
+            f.write("End Time: " + str(start_time + timedelta(minutes=span)) + "\n")
+        f.write("Time Span: " + str(span) + (" minute" if span == 1 else " minutes") + "\n")
+
 def cntExtract(
     project_root: str,
     client: Client,
@@ -31,16 +40,11 @@ def cntExtract(
     # network code is 0101 for HiNet
     data, ctable = client.get_continuous_waveform("0101", starttime, span)
 
+    createMetadataFile(request_timestamp, starttime, span)
+
     # data conversion (WIN32 -> SAC)
     win32.extract_sac(data, ctable) 
     win32.extract_sacpz(ctable)
-
-    # metadata file writing
-    with open("info.txt", "w") as f:
-        f.write("Request Timestamp: " + str(request_timestamp) + "\n")
-        f.write("Start Time: " + str(starttime) + "\n")
-        f.write("End Time: " + str(starttime + timedelta(minutes=span)) + "\n")
-        f.write("Time Span: " + str(span) + (" minute" if span == 1 else " minutes") + "\n")
 
     # go back to root
     os.chdir(project_root)
@@ -66,6 +70,8 @@ def evtExtract(
         region,
         min_magnitude
     )
+
+    createMetadataFile(request_timestamp, starttime, span)
 
     for event in os.listdir("."):
         print(os.getcwd())
