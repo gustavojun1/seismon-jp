27cf4e0f90e1a4d147141980d1c9fbedf9fdb1aa
jun <gustavonagatomo@gmail.com>
2024-12-12 17:53:55 +0900

    feat: Modularize directory handling logic

--- data/extract.py
+++ data/extract.py
@@ -2,25 +2,29 @@ from HinetPy import Client, win32
 from datetime import datetime, timedelta
 import os 
 
+def create_target_dir(project_root: str, request_timestamp: str):
+
+    os.chdir(project_root)
+
+    # the name of the target dir is the date of the extraction request
+    # the final path is: "project/data/continuous_data/[date_of_request]"
+    target_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "continuous_data",
+        request_timestamp
+    )
+    os.makedirs(target_dir)
+    os.chdir(target_dir)
+
 def cnt_extract(
     project_root: str,
     client: Client,
     starttime: datetime,
     span = 5):
 
-    os.chdir(project_root)
-
-    # the name of the target dir is the date of the extraction request
-    # the final path is: "project/data/continuous_data/[date_of_request]"
     request_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    target_dir = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        "continuous_data",
-        request_timestamp
-    )
 
-    os.makedirs(target_dir)
-    os.chdir(target_dir)
+    create_target_dir(project_root, request_timestamp)
 
     # data retrival
     # network code is 0101 for HiNet
@@ -35,7 +39,7 @@ def cnt_extract(
         f.write("Request Timestamp: " + str(request_timestamp) + "\n")
         f.write("Start Time: " + str(starttime) + "\n")
         f.write("End Time: " + str(starttime + timedelta(minutes=span)) + "\n")
-        f.write("Time Span: " + str(span) + ("minute" if span == 1 else "minutes") + "\n")
+        f.write("Time Span: " + str(span) + (" minute" if span == 1 else " minutes") + "\n")
 
     # go back to root
     os.chdir(project_root)
