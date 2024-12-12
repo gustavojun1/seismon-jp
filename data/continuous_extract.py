56e2b954c5b93b347c567f2afc2bee9369658edd
jun <gustavonagatomo@gmail.com>
2024-12-12 17:42:33 +0900

    refactor: Minor code legibility changes

--- data/continuous_extract.py
+++ data/continuous_extract.py
@@ -10,25 +10,32 @@ def cnt_extract(
 
     os.chdir(project_root)
 
-    target_dir = os.path.dirname(os.path.abspath(__file__)) + "/continuous_data"
-
     # the name of the target dir is the date of the extraction request
+    # the final path is: "project/data/continuous_data/[date_of_request]"
     request_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    target_dir = os.path.join(target_dir, request_timestamp)
+    target_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "continuous_data",
+        request_timestamp
+    )
+
     os.makedirs(target_dir)
     os.chdir(target_dir)
 
+    # data retrival
     # network code is 0101 for HiNet
     data, ctable = client.get_continuous_waveform("0101", starttime, span)
 
+    # data conversion (WIN32 -> SAC)
     win32.extract_sac(data, ctable) 
-
     win32.extract_sacpz(ctable)
 
+    # metadata file writing
     with open("info.txt", "w") as f:
         f.write("Request Timestamp: " + str(request_timestamp) + "\n")
         f.write("Start Time: " + str(starttime) + "\n")
         f.write("End Time: " + str(starttime + timedelta(minutes=span)) + "\n")
         f.write("Time Span: " + str(span) + ("minute" if span == 1 else "minutes") + "\n")
 
+    # go back to root
     os.chdir(project_root)
