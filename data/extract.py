54774a2b8ec2a0018f759803a82c92e9315c6266
jun <gustavonagatomo@gmail.com>
2025-01-13 16:15:00 +0900

    feat: Apply conditional data download (max 2 hour span per event)

--- data/extract.py
+++ data/extract.py
@@ -8,7 +8,7 @@ def createTargetDir(project_root: str, data_type: Literal["continuous", "event"]
     os.chdir(project_root)
 
     # the name of the target dir is the date of the extraction request
-    # the final path is: "project/data/continuous_data/[date_of_request]"
+    # the final path is: "project_root/data/[data_type]_data/[date_of_request]"
     target_dir = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         data_type + "_data",
@@ -19,19 +19,53 @@ def createTargetDir(project_root: str, data_type: Literal["continuous", "event"]
 
     return target_dir
 
-def createMetadataFile(request_timestamp, start_time, span, max_span = 0, end_time: bool = True, name = "info.txt"):
+def createCntMetadataFile(request_timestamp, start_time, downloaded_event_list, not_downloaded_event_list, max_span = 0, name = "info.txt"):
 
     with open(name, "w") as f:
+
         f.write("Request Timestamp: " + str(request_timestamp) + "\n")
+        
+        total_downloaded_minutes = 0
+        f.write("Downloaded events:\n")
+        for de in downloaded_event_list:
+            total_downloaded_minutes += de[1]
+            f.write(f"From {de[0]} to {de[0] + timedelta(de[1])}")
+
+        f.write("Not downloaded events:\n")
+        for nde in not_downloaded_event_list:
+            f.write(f"From {nde[0]} to {nde[0] + timedelta(nde[1])}")
+
         f.write("Start Time: " + str(start_time) + "\n")
-        if end_time:
-            f.write("End Time: " + str(start_time + timedelta(minutes=span)) + "\n")
-        f.write("Time Span: " + str(span) + (" minute" if span == 1 else " minutes") + "\n")
+
+        f.write("Total Downloaded Time Span: " + str(total_downloaded_minutes) + (" minute" if total_downloaded_minutes == 1 else " minutes") + "\n")
+
         if max_span > 0:
-            f.write("Sub-request Max Span: " + str(max_span) + (" minute" if span == 1 else " minutes") + "\n")
+            f.write("Sub-request Max Span: " + str(max_span) + (" minute" if max_span == 1 else " minutes") + "\n")
+
+def createEvtMetadataFile(request_timestamp, start_time, span, name = "info.txt"):
+    with open(name, "w") as f:
+
+        f.write(f"Request Timestamp: {str(request_timestamp)}\n")
+
+        f.write(f"Start Time: {start_time}\n")
+
+        f.write(f"Span: {span}\n")
+
+        f.write(f"End Time: {start_time + timedelta(span)}\n")
+
+# D format is the data format that HiNet uses to name the download folder and is basically D + date in question
+# ex.: D20120101000394_20
+def getDateFromDFormat(d_format_string):
+    try:
+        date = d_format_string[1:15]
+        parsed_date = datetime.strptime(date, "%Y%m%d%H%M%S")
+        return parsed_date
+    except ValueError:
+        raise ValueError(f"Invalid date string format: {date}")
 
 def cntExtract(
     project_root: str,
+    events_path_list,
     client: Client,
     starttime: datetime,
     span,
@@ -41,11 +75,49 @@ def cntExtract(
 
     createTargetDir(project_root, "continuous", request_timestamp)
 
-    # data retrival
-    # network code is 0101 for HiNet and 0103 for F-net
-    data, ctable = client.get_continuous_waveform("0103", starttime, span, max_span, threads=8, cleanup=False)
+    events_path_list.sort()
+    prev_event_timestamp = starttime - timedelta(hours = 2 + 1)
 
-    createMetadataFile(request_timestamp, starttime, span, max_span)
+    print("Continuous data download starting...")
+
+    donwloaded_events_list = []
+    not_donwloaded_events_list = []
+
+    # To not overload the network, the continuous data downloaded only occurs in the two 2 previous hours (time period considered to have relevant information for the prediction) before each event.
+    for event in events_path_list:
+
+        event_timestamp = None
+
+        with open(event, "r", encoding="shift_jis") as file:
+            for line in file:
+                if "ORIGIN_JST" in line:
+                    event_timestamp = datetime.strptime(line.split(":", maxsplit=1)[1].strip(), "%Y/%m/%d %H:%M:%S.%f")
+                    print(f"Event timestamp: {event_timestamp}")
+
+        event_starttime = event_timestamp - timedelta(hours=2)
+        event_relevant_span = 120
+
+        # Guarantee it does not get data before starttime
+        if event_starttime < starttime:
+            event_starttime = starttime
+            event_relevant_span  -= (starttime - event_starttime).total_seconds() / 60
+
+        # Prevents downloading data from the same time period twice
+        if event_starttime < prev_event_timestamp:
+            event_starttime = prev_event_timestamp
+
+        print(f"Downloading data from {event_starttime} to {event_starttime + timedelta(minutes=event_relevant_span)}")
+
+        # data retrival
+        # network code is 0101 for HiNet and 0103 for F-net
+        data, ctable = client.get_continuous_waveform("0103", event_starttime, event_relevant_span, max_span, threads=8, cleanup=False)
+
+        if data is not None and ctable is not None:
+            donwloaded_events_list.append([event_starttime, event_relevant_span])
+        else:
+            not_donwloaded_events_list.append([event_starttime, event_relevant_span])
+
+    createCntMetadataFile(request_timestamp, starttime, donwloaded_events_list, not_donwloaded_events_list, max_span)
 
     # data conversion (WIN32 -> SAC)
     win32.extract_sac(data, ctable) 
@@ -66,9 +138,11 @@ def evtExtract(
     request_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     target_dir = createTargetDir(project_root, "event", request_timestamp)
-    target_dir = os.path.join(target_dir, )
 
     endtime = starttime + timedelta(minutes=span)
+
+    print("Event download starting...")
+
     # data retrival
     events = client.get_event_waveform(
         starttime,
@@ -78,21 +152,21 @@ def evtExtract(
         max_magnitude
     )
 
-    createMetadataFile(request_timestamp, starttime, span)
+    createEvtMetadataFile(request_timestamp, starttime, span)
 
     # list of paths to each event to guide the continuous extraction
     events_path_list = []
 
+    print("Extracting events files..")
+
     for event in os.listdir("."):
-        print(os.getcwd())
         if os.path.isdir(event):
-            events_path_list.append(os.path.join(os.getcwd,event))
+            events_path_list.append(os.path.join(target_dir, event, f"{event}.txt"))
             os.chdir(event)
             data = event + ".evt"
             ctable = event + ".ch"
             win32.extract_sac(data, ctable)
             os.chdir("..")
-        # break # for extracting 1 event only for now
 
     # go back to root
     os.chdir(project_root)
